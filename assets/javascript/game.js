bc6af456c6efcf9b4c528f8b227439a7e3bc2add
Logan <loganwswenson@gmail.com>
2019-06-06 19:41:00 -0600

    cleaned up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
     let randomNumber=Math.floor(Math.random()*101+19)
-
-    $('#randomNumber').text(randomNumber);
-
     let Total= 0; 
     let win= 0;
     let lose = 0;
@@ -11,42 +8,42 @@ $(document).ready(function(){
     let flyNum= Math.floor(Math.random()*11+1)
     let rexNum= Math.floor(Math.random()*11+1)
 
-  $('#winCount').text(win);
-  $('#lossCount').text(lose);
+$('#randomNumber').text(randomNumber);
+$('#winCount').text(win);
+$('#lossCount').text(lose);
 
   function reset(){
-        Random=Math.floor(Math.random()*101+19);
-        console.log(randomNumber)
-        $('#randomNumber').text(randomNumber);
-        jirfNum= Math.floor(Math.random()*11+1);
-        rapNum= Math.floor(Math.random()*11+1);
-        flyNum= Math.floor(Math.random()*11+1);
-        rexNum= Math.floor(Math.random()*11+1);
-        Total= 0;
-        $('#total').text(Total);
-        } 
-  //adds the win to the Total
-  function yay(){
-  alert("You won!");
+    Random=Math.floor(Math.random()*101+19);
+    console.log(randomNumber)
+    $('#randomNumber').text(randomNumber);
+    jirfNum= Math.floor(Math.random()*11+1);
+    rapNum= Math.floor(Math.random()*11+1);
+    flyNum= Math.floor(Math.random()*11+1);
+    rexNum= Math.floor(Math.random()*11+1);
+    Total= 0;
+    $('#total').text(Total);
+    } 
+
+  function winner(){
+  alert("Winner!");
     win++; 
     $('#winCount').text(win);
     reset();
   }
-  //addes the lose to the Total
+
   function loser(){
-  alert ("You lose!");
+  alert ("Loser!");
     lose++;
     $('#lossCount').text(lose);
     reset()
   }
-  //sets up click for jewels
+
     $('#jirf').on ('click', function(){
       Total = Total + jirfNum;
       console.log("New Total= " + Total);
       $('#total').text(Total); 
-            //sets win/lose conditions
           if (Total == randomNumber){
-            yay();
+            winner();
           }else if ( Total > randomNumber){
             loser();
           }   
@@ -56,7 +53,7 @@ $(document).ready(function(){
       console.log("New Total= " + Total);
       $('#total').text(Total); 
           if (Total == randomNumber){
-            yay();
+            winner();
           }else if ( Total > randomNumber){
             loser();
           } 
@@ -66,7 +63,8 @@ $(document).ready(function(){
       console.log("New Total= " + Total);
       $('#total').text(Total);
             if (Total == randomNumber){
-            yay();} else if ( Total > randomNumber){
+            winner();
+        } else if ( Total > randomNumber){
             loser();
           } 
     })  
@@ -75,7 +73,7 @@ $(document).ready(function(){
       console.log("New Total= " + Total);
       $('#total').text(Total); 
             if (Total == randomNumber){
-            yay();
+            winner();
 
           }else if ( Total > randomNumber){
             loser();
