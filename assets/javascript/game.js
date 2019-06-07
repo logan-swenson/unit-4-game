f4d49e3632b97e67fb13c1fbe18c80dac61a061e
Logan <loganwswenson@gmail.com>
2019-06-06 22:36:31 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,82 +1,75 @@
 $(document).ready(function(){
-    let randomNumber=Math.floor(Math.random()*101+19)
+    
     let Total= 0; 
-    let win= 0;
     let lose = 0;
-    let jirfNum= Math.floor(Math.random()*11+1)
-    let rapNum= Math.floor(Math.random()*11+1)
-    let flyNum= Math.floor(Math.random()*11+1)
-    let rexNum= Math.floor(Math.random()*11+1)
-
+    let win= 0;
+    let jirfNum= Math.floor(Math.random()*12);
+    let rapNum= Math.floor(Math.random()*12);
+    let flyNum= Math.floor(Math.random()*12);
+    let rexNum= Math.floor(Math.random()*12);
+    let randomNumber=Math.floor(Math.random()*120);
+    
 $('#randomNumber').text(randomNumber);
 $('#winCount').text(win);
 $('#lossCount').text(lose);
 
-  function reset(){
-    Random=Math.floor(Math.random()*101+19);
-    console.log(randomNumber)
+function winner(){
+    alert("Winner!");
+    win++; 
+    $('#winCount').text(win);
+    newGame();}
+
+function loser(){
+    alert ("Loser!");
+    lose++;
+    $('#lossCount').text(lose);
+    newGame()}
+
+function newGame(){
+    Random=Math.floor(Math.random()*120);
+    console.log(randomNumber);
     $('#randomNumber').text(randomNumber);
-    jirfNum= Math.floor(Math.random()*11+1);
-    rapNum= Math.floor(Math.random()*11+1);
-    flyNum= Math.floor(Math.random()*11+1);
-    rexNum= Math.floor(Math.random()*11+1);
+    jirfNum= Math.floor(Math.random()*12);
+    rapNum= Math.floor(Math.random()*12);
+    flyNum= Math.floor(Math.random()*12);
+    rexNum= Math.floor(Math.random()*12);
     Total= 0;
     $('#total').text(Total);
     } 
 
-  function winner(){
-  alert("Winner!");
-    win++; 
-    $('#winCount').text(win);
-    reset();
-  }
-
-  function loser(){
-  alert ("Loser!");
-    lose++;
-    $('#lossCount').text(lose);
-    reset()
-  }
-
     $('#jirf').on ('click', function(){
-      Total = Total + jirfNum;
-      console.log("New Total= " + Total);
-      $('#total').text(Total); 
-          if (Total == randomNumber){
-            winner();
-          }else if ( Total > randomNumber){
-            loser();
-          }   
-    })  
-    $('#rap').on('click', function(){
-      Total = Total + rapNum;
-      console.log("New Total= " + Total);
-      $('#total').text(Total); 
-          if (Total == randomNumber){
-            winner();
-          }else if ( Total > randomNumber){
-            loser();
-          } 
-    })  
-    $('#fly').on('click', function(){
-      Total = Total + flyNum;
-      console.log("New Total= " + Total);
-      $('#total').text(Total);
+        Total = Total + jirfNum;
+        console.log("New Total= " + Total);
+        $('#total').text(Total); 
             if (Total == randomNumber){
             winner();
-        } else if ( Total > randomNumber){
-            loser();
-          } 
-    })  
-    $('#rex').on('click', function(){
-      Total = Total + rexNum;
-      console.log("New Total= " + Total);
-      $('#total').text(Total); 
-            if (Total == randomNumber){
-            winner();
+            }else if ( Total > randomNumber){
+            loser();}})  
 
-          }else if ( Total > randomNumber){
-            loser();
-          }
-    });   
+    $('#rap').on('click', function(){
+        Total = Total + rapNum;
+        console.log("New Total= " + Total);
+        $('#total').text(Total); 
+            if (Total == randomNumber){
+            winner();
+            }else if ( Total > randomNumber){
+            loser();}})  
+
+    $('#fly').on('click', function(){
+        Total = Total + flyNum;
+        console.log("New Total= " + Total);
+        $('#total').text(Total);
+            if (Total == randomNumber){
+            winner();
+            } else if ( Total > randomNumber){
+            loser();}})  
+
+    $('#rex').on('click', function(){
+        Total = Total + rexNum;
+        console.log("New Total= " + Total);
+        $('#total').text(Total); 
+            if (Total == randomNumber){
+            winner();
+            }else if ( Total > randomNumber){
+            loser();}});   
   }); 
